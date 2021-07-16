f0721d23b0b265330d4e40145d204e914ef68d5f
PedroRIHP <pedroangel.rihp@gmail.com>
2021-07-16 00:16:07 -0500

    TryCatch

---
+++ TryCatch/src/Principal.java
@@ -0,0 +1,27 @@
+
+public class Principal {
+	
+	static int numerador = 10;
+	static integer denominador = 0 ;
+	static float division;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		System.out.println("antes de la divisions");
+		try {
+			division = numerador / denominador;	
+		} catch(ArithmeticException ex) {
+			division = 0;
+			System.out.println("error" + ex.getMessage());
+		}
+		
+		System.out.println("Despues de la division");
+	}
+	*/
+	String frutas []= {"mango", "pera", "manzana", "uva"};
+	for (String fruta: frutas) {
+		System.out.println(Fruta);
+	}
+	}
+}
